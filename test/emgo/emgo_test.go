ce33e6e9e3da1366b46ffe7d341aeaabbf3c3dd7
Duong Tan Nhu <duongtannhu@gmail.com>
2019-03-29 22:06:53 +0700

    Add module Emgo

---
+++ test/emgo/emgo_test.go
@@ -0,0 +1,39 @@
+package emgo
+
+import (
+	"testing"
+
+	"github.com/globalsign/mgo/bson"
+	"github.com/go-eco/core"
+	"github.com/go-eco/core/emgo"
+)
+
+type appTestMgo struct {
+	mgo emgo.Emgo
+	t   *testing.T
+}
+
+func (m *appTestMgo) Init()    {}
+func (m *appTestMgo) Cleanup() {}
+func (m *appTestMgo) Run() {
+	db, close := m.mgo.C("user")
+	defer close()
+	err := db.Insert(bson.M{"name": "Nhu", "description": "A software developer"})
+	if err != nil {
+		m.t.Fatal(err)
+	}
+}
+
+func TestEmgo(t *testing.T) {
+	app := core.NewApp()
+	mgo := emgo.GetEmgo("")
+	app.RegisterService(mgo)
+
+	appTest := appTestMgo{
+		mgo: mgo,
+		t:   t,
+	}
+	app.RegisterRunner(&appTest)
+
+	app.Run()
+}
